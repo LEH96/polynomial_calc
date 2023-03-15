bf3b5f2095f7dc3a81fb25cf47b56b0cb1816c2f
LEH96 <lutea67@naver.com>
2023-03-15 16:47:30 +0900

    230315

--- src/main/java/org/example/Polynomial.java
+++ src/main/java/org/example/Polynomial.java
@@ -1,36 +1,66 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Polynomial {
-    String exp;
-    public Polynomial(String s){
-        this.exp = s;
-    }
 
-    public int calc() {
-        String[] in = exp.split(" ");
-        List<Character> sign = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
+    public static int calc(String exp) {
+        exp = exp.trim();
+        if ( !exp.contains(" ") ) return Integer.parseInt(exp);
 
-        for(int i=0;i<in.length;i++){
-            if( Character.isDigit(in[i].charAt(0)) )
-                nums.add(Integer.parseInt(in[i]));
-            else
-                sign.add(in[i].charAt(0));
+        exp = exp.replaceAll("- ", "+ -");
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ");
+        boolean haveBracket = exp.contains("(")||exp.contains(")");
+        boolean needToCompound = needToMulti && needToPlus;
+
+        if( haveBracket ){
+            if(!needToPlus&&!needToMulti){
+                exp = exp.replaceAll("\\(","");
+                exp = exp.replaceAll("\\)","");
+            }
+            String First_exp = exp.split("\\)",2)[0];
+            String Second_exp = exp.split("\\)",2)[1];
+            First_exp = First_exp.replaceAll("\\(","");
+            Second_exp = Second_exp.replaceAll("\\)","");
+
+            String sub_exp = calc(First_exp) + " " + Second_exp.trim();
+            return calc(sub_exp);
+        }
+        if ( needToCompound ) {
+            String[] bits = exp.split(" \\+ ");
+
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Polynomial::calc)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            return calc(newExp);
+        }
+        else if ( needToPlus ) {
+            String[] bits = exp.split(" \\+ ");
+
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+        else if ( needToMulti ) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
         }
 
-        return nums.stream().mapToInt(Integer::intValue).sum();
-//        int result = nums.get(0);
-//        for(char c : sign){
-//            switch(c){
-//                case '+':
-//                case '*':
-//                case '(':
-//            }
-//        }
-//
-//        return result;
+        return -1;
     }
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -7,25 +7,79 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class test {
     @Test
     @DisplayName("3 + 3 == 6")
-    void t5() {
-        assertThat(new Polynomial("3 + 3").calc()).isEqualTo(6);
+    void t1() {
+        assertThat(Polynomial.calc("3 + 3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("13 + 2 == 15")
+    void t2() {
+        assertThat(Polynomial.calc("13 + 2")).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("3 * 3 == 9")
+    void t3() {
+        assertThat(Polynomial.calc("3 * 3")).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("20 + 10 + 5 * 2 == 40")
+    void t4() {
+        assertThat(Polynomial.calc("20 + 10 + 5 * 2")).isEqualTo(40);
+    }
+
+    @Test
+    @DisplayName("50 - 30 => 20")
+    void t5() {
+        assertThat(Polynomial.calc("50 - 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
     void t6() {
-        assertThat(new Polynomial("13 + 2").calc()).isEqualTo(15);
+        assertThat(Polynomial.calc("10 + 20 + 30")).isEqualTo(60);
     }
 
     @Test
-    @DisplayName("3 * 3 + 3 == 12")
+    @DisplayName("10 + 20 - 30 == 0")
     void t7() {
-        assertThat(new Polynomial("3 * 3 + 3").calc()).isEqualTo(12);
+        assertThat(Polynomial.calc("10 + 20 - 30")).isEqualTo(0);
     }
 
     @Test
-    @DisplayName("(3 + 3) * 3 == 18")
+    @DisplayName("10 * 20 - 30 * 10 == -100")
     void t8() {
-        assertThat(new Polynomial("(3 + 3) * 3").calc()).isEqualTo(18);
+        assertThat(Polynomial.calc("10 * 20 - 30 * 10")).isEqualTo(-100);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) == 30")
+    void t9() {
+        assertThat(Polynomial.calc("(10 + 20)")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("10 + ((20 - 10)) == 20")
+    void t10() {
+        assertThat(Polynomial.calc("10 + (20 - 10)")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("((10 + 20)) == 30")
+    void t11() {
+        assertThat(Polynomial.calc("((10 + 20))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) * 3 == 90")
+    void t12() {
+        assertThat(Polynomial.calc("(10 + 20) * 3")).isEqualTo(90);
+    }
+
+    @Test
+    @DisplayName("10 + (10 + 5) == 25")
+    void t13() {
+        assertThat(Polynomial.calc("10 + (10 + 5)")).isEqualTo(25);
     }
 }
